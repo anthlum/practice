dd5aaa63f1e3babbce49ebf5fd984e04c5617e6d
anthlum <134817528+anthlum@users.noreply.github.com>
2023-05-29 20:53:03 +0900

    Create practice_one.js

---
+++ practice_one.js
@@ -0,0 +1,13 @@
+function practAA() {
+  document.getElementById('demo').innerHTML= 'Paragraph changed';
+}
+function practBB() {
+  document.getElementsByName('opinion')[0].style.color= '#46a';
+}
+let x, y, z;
+x= 5;
+y= 6;
+z= x + y;
+function practCC() {
+  document.getElementById('demo-aa').innerText= 'The value of z is' + z + '.'
+}
