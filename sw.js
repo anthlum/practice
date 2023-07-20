0cbc1a38213c1d1204f26c3ab9e25140cdf5de00
anthlum <134817528+anthlum@users.noreply.github.com>
2023-07-20 14:08:03 +0900

    modify service work

--- sw.js
+++ sw.js
@@ -1,48 +1,22 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('video-store').then(function(cache) {
-      return cache.addAll([
+
+self.addEventListener('activate', (evt) => {
+  console.log('Service worker is active.');
+})
+self.addEventListener('install', (evt) => {
+  console.log('Install service worker...');
+  evt.waitUntil(
+    caches.open('cacheProx')
+    .then((cache) => cache.addAll([
       '/practice/',
       '/practice/videoDB.html',
       '/practice/videoDB.css',
-      '/practice/videoDB.js'
-    ]);
-    })
-  );
+      '/practice/videoDB.js'])
+      ));
 });
-
-self.addEventListener('fetch', e => {
-    console.log(e.request.url);
-    e.respondWith(
-      caches.match(e.request).then(response => response || fetch(e.request))
-    );
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(chache.match(evt.request)
+  .then((response) => {
+    console.log('Cache response the request; '+ evt.request.url);
+    return response || fetch(evt.request);
   });
-
-// self.addEventListener('activate', (evt) => {
-//   console.log('Service worker is active.');
-// })
-// self.addEventListener('install', (evt) => {
-//   console.log('Install service worker...');
-//   evt.waitUntil(
-//     caches.open('cacheProx')
-//     .then((cache) => cache.addAll([
-//       '/mdnproject/',
-//       '/mdnproject/videoDB.html',
-//       '/mdnproject/videoDB.css',
-//       '/mdnproject/videoDB.js'])
-//       ));
-// });
-// self.addEventListener('fetch', (evt) => {
-//   evt.respondWith(chache.match(evt.request)
-//   .then((response) => {
-//     console.log('Caqhe response the request; '+ evt.request.url);
-//     return response || fetch(evt.request)
-//     .then((resp) => caches.open('cacheProx')
-//       .then((cache) => {
-//         cache.put(evt.request, resp.clone());
-//         console.log('Cache new resource: ' + evt.request.url);
-//         return resp;
-//       }));
-//   })
-//   );
-// });
+});
