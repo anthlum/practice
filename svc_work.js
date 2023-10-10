8786551e77effb09766f050c144a6d7f6d06188e
anthlum <134817528+anthlum@users.noreply.github.com>
2023-10-11 08:56:01 +0900

    Update svc_work.js
    try async

--- svc_work.js
+++ svc_work.js
@@ -13,39 +13,46 @@ const shellFiles = [
   '/practice/images/pic4.jpg',
   '/practice/images/pic5.jpg'
 ];
-const installSvc = async () => {
-    const cache = await caches.open(cacheName);
-    return cache.addAll(shellFiles);
+const installSvc = (evt) => {
+  console.log('Install service worker...', cacheName);
+  evt.waitUntil(async () => {
+    try {
+      const cache = await caches.open(cacheName);
+      return cache.addAll(shellFiles);
+    }
+    catch { console.log('error during installing');
+    }
+  }); 
 }
-const activeSvc = async () => {
-  const nameSet = await caches.keys();
-  return Promise.all(nameSet.map((keyName) => {
-    if(keyName.indexOf(cacheName) < 0) console.log(keyName)
-  }));
+const activeSvc = () => {
+  evt.waitUntil(async () => {
+    try {
+      const nameSet = await caches.keys();
+      console.log('Service worker is active.');
+      return Promise.all(nameSet.map((keyName) => {
+        if(keyName.indexOf(cacheName) < 0) {
+          console.log(keyName);
+        }}
+      ));
+      }
+    catch { console.log('error during activing', nameSet)
+    }});
+  }
+const fetchSvc = (evt) => {
+  evt.respondWith(async () => {
+    try {
+      const response = await caches.match(evt.request);
+      if(response) {
+        console.log('Cached resource: ' + evt.request.url);
+        return response; }
+      console.log('New resources: ' + evt.rquest.url);
+      const reply = await fetch(evt.request);
+      return reply;
+    }
+    catch { console.log('error during activing', evt.request.url);
+    }
+  });
 }
-const fetchSvc = async (evt) => {
-  console.log(evt.request.url);
-  const response = await caches.open(evt.request);
-  if(response) {
-    console.log('Cached resource: ' + evt.request.url);
-    return response; }
-  console.log('New resources: ' + evt.rquest.url);
-  const reply = await fetch(evt.request);
-  return reply; 
-}
-self.addEventListener('install', (evt) => {
-  // eventTarget > Worker(GlobalScope) > ServiceWorker(GlobalScope) = self > clients
-  console.log('Install service worker...', caches);
-  evt.waitUntil(installSvc());
-    // cacheStorage(caches) method - open(), match(), delete(), has(), keys()
-    // make a named cacheInstace in a cacheStorage
-    // cacheInstance method - add(), addAll(), delete(), keys(), match(), matchAll(), put()
-});
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(activeSvc());
-  console.log('Service worker is active.');
-})
-self.addEventListener('fetch', (evt) => {
-  evt.respondWith(fetchSvc(evt));
-});
-
+self.addEventListener('install', installSvc(evt));
+self.addEventListener('activate', activeSvc());
+self.addEventListener('fetch', fetchSvc(evt));
