70c9ea3ea14399a07ce7a0a7606a1e1e2cbf1106
anthlum <134817528+anthlum@users.noreply.github.com>
2023-10-12 16:53:16 +0900

    Update svc_work.js
    fuction00

--- svc_work.js
+++ svc_work.js
@@ -13,36 +13,36 @@ const shellFiles = [
   '/practice/images/pic4.jpg',
   '/practice/images/pic5.jpg'
 ];
-const installSvc = async () => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(shellFiles);
-}
-const activeSvc = async () => {
-  const nameSet = await caches.keys();
-  nameSet.map((keyName) => { // return Promise.all()
-    if(keyName.indexOf(cacheName) < 0) console.log(keyName, nameSet)
+const installSvc = (evt) => {
+  console.log('Install service worker...', cacheName);
+  evt.waitUntil(async () => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(shellFiles);
   });
 }
-const fetchSvc = async (evt) => {
-  const cache = await caches.open(cacheName);
-  const response = await cache.match(evt.request);
-  if(response) {
-    console.log('Cached resource: ' + evt.request.url);
-    return response; }
-  console.log('New resources: ' + evt.request.url);
-  const reply = await fetch(evt.request);
-  // await cache.put(evt.request, reply.clone());
-  return reply; 
-}
-self.addEventListener('install', (evt) => {
-  console.log('Install service worker...', cacheName);
-  evt.waitUntil(installSvc());
-});
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(activeSvc());
+const activeSvc = (evt) => {
+  evt.waitUntil(async () => {
+    const nameSet = await caches.keys();
+    nameSet.map((keyName) => { // return Promise.all()
+      if(keyName.indexOf(cacheName) < 0) console.log(keyName, nameSet)
+    });
+  });
   console.log('Service worker is active.');
-})
-  self.addEventListener('fetch', (evt) => {
+}
+const fetchSvc = (evt) => {
   if(evt.request.url.indexOf('http') < 0) return;
-  evt.respondWith(fetchSvc(evt));
-});
+  evt.respondWith(async (evt) => {
+    const cache = await caches.open(cacheName);
+    const response = await cache.match(evt.request);
+    if(response) {
+      console.log('Cached resource: ' + evt.request.url);
+      return response; }
+    console.log('New resources: ' + evt.request.url);
+    const reply = await fetch(evt.request);
+    // await cache.put(evt.request, reply.clone());
+    return reply; 
+  });
+}
+self.addEventListener('install', installSvc);
+self.addEventListener('activate', activeSvc);
+self.addEventListener('fetch', fetchSvc);
